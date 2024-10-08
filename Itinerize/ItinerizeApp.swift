682b3d073f886978e957dee4468962c9e1f353e4
DevMoodie <Mahmoudyf96@gmail.com>
2024-10-08 15:27:34 -0400

    Initial Commit

---
+++ Itinerize/ItinerizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ItinerizeApp.swift
+//  Itinerize
+//
+//  Created by Moody on 2024-10-08.
+//
+
+import SwiftUI
+
+@main
+struct ItinerizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
